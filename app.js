1b4803a7e4f7cf5b52ed10ec8a6651a4c61092bc
George Moon <george.moon@gmail.com>
2013-07-15 17:36:28 -0400

    tweaked the app.use call to provide only a function for the root

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use("/", express.static(__dirname));
+app.use(express.static(__dirname));
 
 app.listen(4444);
 console.log("listening on port 4444");
