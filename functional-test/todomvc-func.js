e228d0f3c7025ac9e78ec28fd764012fc5d0069e
George Moon <george.moon@gmail.com>
2013-07-23 17:46:07 -0400

    fixed jshint complaints in todomvc-func.js

--- functional-test/todomvc-func.js
+++ functional-test/todomvc-func.js
@@ -1,12 +1,14 @@
+/* jshint node:true */
 /* global equal:true, module:true, S:true, test:true */
+'use strict';
 
-module("skolr client: agilityjs", {
-	setup: function() {
+module('skolr client: agilityjs', {
+	setup: function () {
 		S.open('http://localhost:4444/architecture-examples/agilityjs/index.html');
 	}
 });
 
-test("add new todo",function(){
-	S('#new-todo').click().type("make sure skolr works");
-	equal( S('#todo-list>li').size(), 1, "there is 1 todo item");
+test('add new todo', function () {
+	S('#new-todo').click().type('make sure skolr works');
+	equal(S('#todo-list>li').size(), 1, 'there is 1 todo item');
 });
